308c7a7ef8b94ceb3dd62ef7e1025501086f8d2e
Hannah Fairlie <2204543F@student.gla.ac.uk>
2018-01-26 14:34:15 +0000

    Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -37,13 +37,13 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category = c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
--- rango/views.py
+++ rango/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rango.models import Category
+from django.http import HttpResponse
 from rango.models import Page
 
-# Create your views here.
+def index(request):
+    page_list = Page.objects.order_by('-views')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+    return render(request, 'rango/index.html', context_dict)
+ 
+
+def about(request):
+    context_dict = {'message': "This tutorial has been put together by Hannah Fairlie."}
+    
+    return render(request, 'rango/about.html', context=context_dict)
+
 def show_category(request, category_name_slug):
+
     context_dict = {}
-
+    
     try:
-        category = Category.objects.get(slug = category_name_slug)
-
+        category = Category.objects.get(slug=category_name_slug)
+        
         pages = Page.objects.filter(category=category)
+        
         context_dict['pages'] = pages
-        context_dict['categories'] = category
 
+        context_dict['category'] = Category
+        
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-        
 
     return render(request, 'rango/category.html', context_dict)
-
-def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-    return render(request, 'rango/index.html', context_dict)
-
-def about(request):
-    context_dict = {}
-    return render(request, 'rango/about.html', context_dict)
-
